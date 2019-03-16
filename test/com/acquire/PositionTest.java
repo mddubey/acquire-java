f677409b8e09b20255e005b658c972c7830abf18
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-03-16 10:33:47 +0530

    Created tests for Position class.

--- test/com/acquire/PositionTest.java
+++ test/com/acquire/PositionTest.java
@@ -1,5 +1,6 @@
 package com.acquire;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,13 +8,59 @@ import static org.junit.jupiter.api.Assertions.*;
 class PositionTest {
 
     @Test
+    @DisplayName("should return false for the one that is not a neighbour(which is not side to that)")
     void isNeigbouringPosition() {
         Position position = new Position(3, 3);
-        Position positionToCheck = new Position(2,3);
+        Position positionToCheck = new Position(2, 2);
         assertFalse(position.isNeigbouringPosition(positionToCheck));
     }
 
+
     @Test
-    void isSamePosition() {
+    @DisplayName("should return true for the one that is right to the position")
+    void isNeigbouringPosition1() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(3, 4);
+        assertTrue(position.isNeigbouringPosition(positionToCheck));
+    }
+
+    @Test
+    @DisplayName("should return true for the one that is left to the position")
+    void isNeigbouringPosition2() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(3, 2);
+        assertTrue(position.isNeigbouringPosition(positionToCheck));
+    }
+
+    @Test
+    @DisplayName("should return true for the one that is top to the position")
+    void isNeigbouringPosition3() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(2, 3);
+        assertTrue(position.isNeigbouringPosition(positionToCheck));
+    }
+
+    @Test
+    @DisplayName("should return true for the one that is bottom to the position")
+    void isNeigbouringPosition4() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(4, 3);
+        assertTrue(position.isNeigbouringPosition(positionToCheck));
+    }
+
+    @Test
+    @DisplayName("should return true for the one which is having same position")
+    void isSamePostion() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(3, 3);
+        assertTrue(position.isSamePosition(positionToCheck));
+    }
+
+    @Test
+    @DisplayName("should return false for the one which is having another position which is not same")
+    void isSamePostion1() {
+        Position position = new Position(3, 3);
+        Position positionToCheck = new Position(3, 2);
+        assertFalse(position.isSamePosition(positionToCheck));
     }
 }
